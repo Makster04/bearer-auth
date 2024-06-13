bcdb9269bae797e50af2b8ea5c3bc3e05ccc02d1
MAK TRNKA <maktrnka@comcast.net>
2024-06-12 18:17:46 -0700

    Completion of Lab 07 for regrading

---
+++ src/auth/middleware/basic.js
@@ -0,0 +1,21 @@
+'use strict';
+
+const base64 = require('base-64');
+const { users } = require('../models/index.js');
+
+module.exports = async (req, res, next) => {
+  if (!req.headers.authorization) { next("Unauthorized"); }
+
+  let basic = req.headers.authorization;
+  let [username, password] = base64.decode(basic.split(' ')[1]).split(':');
+  console.log(username, password)
+  try {
+    req.user = await users.authenticateBasic(username, password) 
+    console.log(req.user)
+    next();
+  } catch (e) {
+    console.error(e); 
+    res.status(403).send('Invalid Login');
+  }
+
+}
